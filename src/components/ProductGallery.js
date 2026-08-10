6427cd47c53cf8c80bb550bb0ff713e394c91350
Gordela <129328566+GordelaG@users.noreply.github.com>
2026-08-09 21:38:08 -0300

    Remove autoplay parameter from YouTube embeds
    Removed the ?autoplay=1 query parameter from YouTube iframe embed URLs in ProductGallery. This prevents videos from automatically playing when the component loads, improving user experience and page performance.

--- src/components/ProductGallery.js
+++ src/components/ProductGallery.js
@@ -69,7 +69,7 @@ export default function ProductGallery({ product }) {
                   <iframe 
                     width="100%" 
                     height="100%" 
-                    src={`https://www.youtube.com/embed/${item.url}?autoplay=1`} 
+                    src={`https://www.youtube.com/embed/${item.url}`} 
                     title="YouTube video player" 
                     frameBorder="0" 
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
